6bee6fe7751e057b019a7019fcb19dfdd9c671d4
Anderson Balbino <andersonbalbino2212@outlook.com>
2023-11-25 21:41:42 -0300

    Criando uma tabuada e explorando o gulp para compilar o sass

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const maps = require('gulp-sourcemaps');
+
+function compilaSass(){
+    return gulp.src('./src/styles/*.scss')
+    .pipe(maps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(maps.write('./maps'))
+    .pipe(gulp.dest('./build/styles'));
+}
+
+
+exports.sass = compilaSass;
+exports.watch = function(){
+    gulp.watch('./src/styles/*.scss', {ignoreInital: false}, gulp.series(compilaSass));
+}
